473489f70a75d67200a22eda4758876968c4541b
IFOODznx <santosfigueiredo269@gmail.com>
2026-03-15 14:51:21 -0300

    Update Manzana.cs

--- Jogo do Crie Seus Jogos/Assets/Scripts/Manzana.cs
+++ Jogo do Crie Seus Jogos/Assets/Scripts/Manzana.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Manzana : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
